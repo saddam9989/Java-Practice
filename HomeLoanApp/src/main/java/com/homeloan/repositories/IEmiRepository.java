0d87d54483fde3716f1bc1c78d9b20d17340071b
saddam9989 <94597886+saddam9989@users.noreply.github.com>
2022-11-15 12:32:00 +0530

    Delete HomeLoanApp directory

--- HomeLoanApp/src/main/java/com/homeloan/repositories/IEmiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.homeloan.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.homeloan.entities.EMI;
-
-public interface IEmiRepository extends JpaRepository<EMI, Integer>{
-
-}
--- HomeLoanApp/src/main/java/com/homeloan/repositories/ILoanApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.homeloan.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.homeloan.entities.LoanApplication;
-
-public interface ILoanApplicationRepository extends JpaRepository<LoanApplication, Integer> {
-	
-}
